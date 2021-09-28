b3bb1cc6cad8aa6ec3b6f7b64c658bda2004c4ff
wwp1122 <37684921+wwp1122@users.noreply.github.com>
2021-09-28 15:59:00 +0800

    Add code

---
+++ WVThreadTimer.cpp
@@ -0,0 +1,41 @@
+﻿#include "WVThreadTimer.h"
+
+
+WVThreadTimer::WVThreadTimer(QObject *parent)
+    : QObject(parent)
+{
+    timer_ = new QTimer;
+    timer_->moveToThread(&thread_);
+
+    connect(timer_, &QTimer::timeout, [&]() { emit Timeout(); });
+    connect(&thread_, &QThread::finished, timer_, &QObject::deleteLater);
+
+    connect(this, SIGNAL(StartTimer(int)), timer_, SLOT(start(int)), Qt::BlockingQueuedConnection);
+    connect(this, &WVThreadTimer::StopTimer, timer_, &QTimer::stop, Qt::BlockingQueuedConnection);
+
+    thread_.start();
+}
+
+WVThreadTimer::~WVThreadTimer()
+{
+    Stop();
+    timer_->deleteLater();
+
+    if (thread_.isRunning()) {
+        thread_.quit();
+        thread_.wait();
+    }
+}
+
+void WVThreadTimer::SetSingleShot(bool is_single) {
+    timer_->setSingleShot(is_single);
+}
+void WVThreadTimer::Start(int msec) {
+    emit StartTimer(msec);
+}
+void WVThreadTimer::Stop() {
+    emit StopTimer();
+}
+bool WVThreadTimer::IsActive() {
+    return timer_->isActive();
+}
---
+++ WVThreadTimer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QTimer>
+#include <QThread>
+
+//Only for use in multi thread
+
+class WVThreadTimer : public QObject
+{
+    Q_OBJECT
+
+public:
+    WVThreadTimer(QObject *parent = nullptr);
+    ~WVThreadTimer();
+
+    void SetSingleShot(bool is_Single);
+    void Start(int msec);
+    bool IsActive();
+    void Stop();
+
+private:
+    QTimer* timer_;
+    QThread thread_;
+
+signals:
+    void StartTimer(int);
+    void StopTimer();
+    void Timeout();
+};
